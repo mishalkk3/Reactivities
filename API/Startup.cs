22dd61ead74a96afceee97766e9eb222b6f6b66b
mishalkk3 <mishalkk366@gmail.com>
2021-12-13 12:24:13 +0530

    CRED in client-app

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using AutoMapper;
+using API.Extensions;
 
 namespace API
 {
@@ -33,23 +34,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => 
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_config);
             
         }
 
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activity: Activity | undefined;
+    closeForm: () => void;
+    createOrEdit: (activity : Activity) => void;
+}
+
+//activity:selectedActivity can use selectedActivity to reference activity in Props
+export default function ActivityForm({activity:selectedActivity , closeForm, createOrEdit}: Props) {
+    //if activity is null
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: ''
+    }
+
+    const [activity, setActivity] = useState(initialState);
+
+    function handleSubmit() {
+        createOrEdit(activity);
+    }
+
+    //react track input
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const {name, value} = event.target;
+        // ...activity spread existing ppty
+        setActivity({...activity, [name]: value})
+    }
+
+    return (
+        <Segment clearing>
+            <Form onSubmit={handleSubmit} autoComplete='off' >
+                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
+                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
+                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
+                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
+                <Button floated='right' positive type='submit' content='Submit' />
+                <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
+            </Form>
+        </Segment>
+    )
+}
